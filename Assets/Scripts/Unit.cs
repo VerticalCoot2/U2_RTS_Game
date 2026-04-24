87bafac5f667730b9876c7ca998a3448df8f403f
verticalcoot2 <verticalcoot2@gmail.com>
2026-04-24 14:42:00 +0200

    rtsGam

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : UnitClass
+{
+    public Unit()
+    {
+        name = "Unity";
+        health = 100;
+        damage = 50;
+        speed = 3.5f;
+    }
+}
---
+++ Assets/Scripts/UnitClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitClass
+{
+    public string name;
+    public int health;
+    public int damage;
+    public float speed;
+}
